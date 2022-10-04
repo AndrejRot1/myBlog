3a2d0db10f350549f8577ab3ea1cd5b929b13be1
andrejrot <andrej.rot1@gmail.com>
2022-10-04 09:10:44 +0200

    new blog

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import sqlite3
 from werkzeug.exceptions import abort
+import json
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
+
+
+
+def check():
+    with open('users.json') as f:
+        users = json.load(f)
+
+    for index in users:
+        if (index['name']== username) and (index['password'] == password):
+            print(index['name'],username,index['password'],password)
+            return True
+
+    print(index['name'],username,index['password'],password)
+    return False
 
 
 def get_db_connection():
@@ -22,10 +40,16 @@ def get_post(post_id):
 
 @app.route('/')
 def index():
+    try:
+        username
+    except:
+        username = 'andrej'
+    print(username)
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html',posts=posts)
+    posts.reverse()
+    return render_template('index.html',posts=posts,username = username)
 
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -34,23 +58,34 @@ def post(post_id):
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    test_user = check()
+    if test_user == False:
+            flash('No user')
+            return redirect(url_for('index'))
+
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
 
         if not title:
             flash('Title is required!')
+
         else:
+
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',(title, content))
             conn.commit()
             conn.close()
-            return redirect(url_for('index'))
+        return redirect(url_for('index'))
     return render_template('create.html')
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
+    test_user = check()
+    if test_user == False:
+            flash('No user')
+            return redirect(url_for('index'))
+
     post = get_post(id)
 
     if request.method == 'POST':
@@ -78,4 +113,36 @@ def delete(id):
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+
+    if request.method == 'POST':
+         global username
+         global password
+         username = request.form['uname']
+         password = request.form['psw']
+
+
+
+         if (not username) or (not password):
+             flash('fild is required!')
+         else:
+             test = check()
+             print(test)
+             if test == True:
+                 return redirect(url_for('index'))
+             if test == False:
+                 return render_template('login.html')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    global username
+    global password
+    username = ''
+    password = ''
+    print(username,password)
     return redirect(url_for('index'))
